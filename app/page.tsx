0bc774e7763f01d7945fff37d1aaf98cbb9ee8c1
Mayank Gupta <mayankgupta14042000@gmail.com>
2023-02-01 12:52:41 +0530

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function HomePage() {
+    return (
+        <div>
+            <h1 className="text-4xl">Welcome to the blog</h1>
+            <p>Some control</p>
+        </div>
+    )
+}
